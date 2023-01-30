ab3e0846b9ff5e7000817dfde212b913f17b00f3
gezi-lzq <lzqtxwd@gamil.com>
2023-01-30 14:45:31 +0800

    add database operations for user table

---
+++ pkg/constants/constant.go
@@ -0,0 +1,8 @@
+package constants
+
+const (
+	EtcdAddress = "127.0.0.1:2379"
+
+	UserTableName   = "user"
+	MySQLDefaultDSN = "xxxx:xxxx@tcp(1.15.222.156:xxxx)/douyin?charset=utf8&parseTime=True&loc=Local"
+)
