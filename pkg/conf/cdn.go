a39e0bc4d3648e387207a26b354bbda1b30a2cf0
ivan1218zhang <1742874952@qq.com>
2023-02-06 00:07:12 +0800

    修改cdn配置

--- pkg/conf/cdn.go
+++ pkg/conf/cdn.go
@@ -4,7 +4,6 @@ type cdn struct {
 	AccessKey string
 	SecretKey string
 	Bucket    string
-	UploadUrl string
 	Url       string
 	LocalPath string
 }
@@ -12,8 +11,7 @@ type cdn struct {
 var CDN = cdn{
 	AccessKey: "SHFEDQXdwn4MO7-4OvW92Qr0LXzEeR-b7si5gpWW",
 	SecretKey: "cbCu0I13O2fLIQq8DbLnhUNydDvNdyaSiAJSwnJj",
-	Bucket:    "dousheng2022",
-	UploadUrl: "https://rcnh4fg5p.hn-bkt.clouddn.com/",
+	Bucket:    "douyin-6379",
 	Url:       "https://cdn.nobugnolife.com/",
-	LocalPath: "tmp/videos/",
+	LocalPath: "./tmp/videos/",
 }
