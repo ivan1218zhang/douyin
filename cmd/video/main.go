730b5c6a594e87286017b33893f833dca14a90b4
ivan1218zhang <1742874952@qq.com>
2023-02-06 00:18:56 +0800

    main添加初始化

--- cmd/video/main.go
+++ cmd/video/main.go
@@ -2,13 +2,20 @@ package main
 
 import (
 	video "douyin/kitex_gen/video/videoservice"
+	"douyin/pkg/conf"
 	"douyin/pkg/db"
 	"log"
+	"os"
 )
 
 func main() {
 	// init数据库
 	err := db.Init()
+	if err != nil {
+		return
+	}
+	// 创建存放视频的文件夹
+	err = os.MkdirAll(conf.CDN.LocalPath, os.ModePerm)
 	if err != nil {
 		return
 	}
