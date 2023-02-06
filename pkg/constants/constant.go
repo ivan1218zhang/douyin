21aab6661d8b87021de8271574c32b3824880586
xsj <seajunxie@163.com>
2023-02-06 11:22:01 +0800

    add api层

--- pkg/constants/constant.go
+++ pkg/constants/constant.go
@@ -16,9 +16,10 @@
 package constants
 
 const (
-	VideoTableName          = "video"
-	MySQLDefaultDSN         = "root:root@tcp(1.15.222.156:3307)/gorm?charset=utf8&parseTime=True&loc=Local"
-	EtcdAddress             = "127.0.0.1:2379"
-	CPURateLimit    float64 = 80.0
-	DefaultLimit            = 10
+	VideoTableName           = "video"
+	MySQLDefaultDSN          = "root:root@tcp(1.15.222.156:3307)/gorm?charset=utf8&parseTime=True&loc=Local"
+	EtcdAddress              = "127.0.0.1:2379"
+	VideoServiceName         = "video_service"
+	CPURateLimit     float64 = 80.0
+	DefaultLimit             = 10
 )
