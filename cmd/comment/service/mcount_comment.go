34a13ed7e7c8835cbb4bc7a588b53f907576c57e
gezi-lzq <lzqtxwd@gamil.com>
2023-02-20 15:48:57 +0800

    增加MCountComment

---
+++ cmd/comment/service/mcount_comment.go
@@ -0,0 +1,33 @@
+package service
+
+import (
+	"context"
+	"douyin/cmd/comment/dal/db"
+	"douyin/cmd/comment/dal/redis"
+	"douyin/kitex_gen/comment"
+)
+
+type MCountCommentService struct {
+	ctx context.Context
+}
+
+func NewMCountCommentService(ctx context.Context) *MCountCommentService {
+	return &MCountCommentService{ctx: ctx}
+}
+
+func (s *MCountCommentService) MCountComment(req *comment.MCountCommentReq) ([]int64, error) {
+	counts := make([]int64, 0)
+
+	for i, id := range req.VideoIdList {
+		count, err := redis.GetCommentCount(s.ctx, id)
+		if err == nil {
+			counts[i] = count
+		}
+		count, err = db.GetCommentCountByVideoID(s.ctx, id)
+		if err == nil {
+			redis.SetCommentCount(s.ctx, id, count)
+			counts[i] = count
+		}
+	}
+	return counts, nil
+}
