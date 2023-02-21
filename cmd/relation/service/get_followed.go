fd937e362d6b8383931e4713a6296ce1e3442905
gezi-lzq <lzqtxwd@gamil.com>
2023-02-22 00:59:41 +0800

    增加getFollow 与 getFollower

---
+++ cmd/relation/service/get_followed.go
@@ -0,0 +1,30 @@
+package service
+
+import (
+	"context"
+	"douyin/cmd/relation/dal/db"
+	"douyin/cmd/video/rpc"
+	"douyin/kitex_gen/common"
+	"douyin/kitex_gen/relation"
+	"douyin/kitex_gen/user"
+)
+
+type GetFollowedService struct {
+	ctx context.Context
+}
+
+func NewGetFollowedService(ctx context.Context) *GetFollowedService {
+	return &GetFollowedService{ctx: ctx}
+}
+
+func (s *GetFollowedService) GetFollowedUser(req *relation.MGetFollowReq) ([]*common.User, error) {
+	res, err := db.GetFollowedUser(s.ctx, req.UserId)
+	if err != nil {
+		return nil, err
+	}
+	userList, err := rpc.MGetUser(s.ctx, &user.MGetUserReq{IdList: res, UserId: req.UserId})
+	if err != nil {
+		return nil, err
+	}
+	return userList, nil
+}
